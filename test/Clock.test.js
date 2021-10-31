5caa11c827792c4c2b2f160f67ba8b4114f60b22
Eduardo <edutraquino@gmail.com>
2021-10-31 19:28:19 -0300

    add more tests

--- test/Clock.test.js
+++ test/Clock.test.js
@@ -4,10 +4,52 @@ describe("Creating the clock", () => {
   test("The clock is created with the right values", () => {
     const clock = new Clock(10);
     expect(clock.getTimeinSeconds()).toBe(600);
+
+    const clock2 = new Clock(5);
+    expect(clock2.getTimeinSeconds()).toBe(300);
   });
 
   test("The clock is created paused", () => {
     const clock = new Clock(10);
+    expect(clock.getTimeIsCounting()).toBeFalsy();
+  });
+});
+
+describe("Clock behaviour", () => {
+  test("Starting the clock", () => {
+    const clock = new Clock(10);
+    clock.startClock();
+
+    expect(clock.getTimeIsCounting()).toBeTruthy();
+  });
+
+  test("Pausing the clock", () => {
+    const clock = new Clock(10);
+    clock.pauseClock();
+
+    expect(clock.getTimeIsCounting()).toBeFalsy();
+  });
+
+  test("Starting and pausing the clock", () => {
+    const clock = new Clock(10);
+    clock.startClock();
+    clock.pauseClock();
+
+    expect(clock.getTimeIsCounting()).toBeFalsy();
+
+    clock.startClock();
+
+    expect(clock.getTimeIsCounting()).toBeTruthy();
+  });
+
+  test("Starting and pausing the clock using toggle", () => {
+    const clock = new Clock(10);
+    clock.toggleCounting();
+
+    expect(clock.getTimeIsCounting()).toBeTruthy();
+
+    clock.toggleCounting();
+
     expect(clock.getTimeIsCounting()).toBeFalsy();
   });
 });
