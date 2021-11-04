96ef0bfd1920ffe7a56cc4beb7174067e09b75ce
Eduardo Aquino <edutraquino@gmail.com>
2021-11-04 17:37:41 -0300

    add documentation

--- dist/buttonsFunctions.js
+++ dist/buttonsFunctions.js
@@ -1,3 +1,8 @@
+/**
+ * Funtion thats returns the innetHtmlText to the start button
+ * @param clockIsRunning
+ * @returns
+ */
 export var updateStartButton = function (clockIsRunning) {
     if (clockIsRunning)
         return "⏸️ Pause Counter";
--- src/buttonsFunctions.ts
+++ src/buttonsFunctions.ts
@@ -1,5 +1,9 @@
 
-
+/**
+ * Funtion thats returns the innetHtmlText to the start button
+ * @param clockIsRunning
+ * @returns 
+ */
 export const updateStartButton = (clockIsRunning: boolean) => {
 
     if (clockIsRunning) return "⏸️ Pause Counter";
