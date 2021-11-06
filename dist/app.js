8c5e7f2150fca7d887dcef95a428c32ffbf59a10
Eduardo Aquino <edutraquino@gmail.com>
2021-11-06 17:35:19 -0300

    dist bundle

--- dist/app.js
+++ dist/app.js
@@ -1,38 +1,29 @@
+import { button__start } from "./buttons/Buttons.js";
 import { openConfigurationsWindow } from "./configurations.js";
 import { EventEmitter } from "./core/EventEmitter.js";
 import Clock from "./model/Clock.js";
-import { appPaused } from "./status/appPaused.js";
-var openConfigurationsButton = document.getElementById("button__configurations");
 var app = document.getElementById("app");
 var clock = new Clock(25);
-var appStatus = appPaused;
-appStatus.onChange(clock);
 // View
 setInterval(updateView, 1000);
 function updateView() {
     app.textContent = clock.getTime();
 }
 // Listening to events
-EventEmitter.listen("ChangeStatus", function (newAppStatus) {
-    appStatus = newAppStatus;
-    appStatus.onChange(clock);
-});
 EventEmitter.listen("ChangeClock", function (initialMinuts) {
     clock.changeInitialMinuts(initialMinuts);
 });
 EventEmitter.listen("StartClock", function () {
+    button__start.innerHTML = "⏸️";
     clock.startClock();
 });
 EventEmitter.listen("PauseClock", function () {
+    button__start.innerHTML = "▶️";
     clock.pauseClock();
 });
 EventEmitter.listen("RestartClock", function () {
     clock.restartClock();
 });
-// Listening to buttons click
-document.querySelectorAll(".button").forEach(function (button) {
-    button.addEventListener("click", function () { return appStatus[button.id](clock); });
-});
-openConfigurationsButton.addEventListener("click", function () {
+EventEmitter.listen("OpenConfigurations", function () {
     openConfigurationsWindow();
 });
--- dist/buttons/Buttons.js
+++ dist/buttons/Buttons.js
@@ -1,7 +1,11 @@
 import { EventEmitter } from "../core/EventEmitter.js";
 export var button__start = document.getElementById("button__start");
-export var button__focus = document.getElementById("button__focus");
-export var button__relax = document.getElementById("button__relax");
 export var button__end = document.getElementById("button__end");
-EventEmitter.listen("StartClock", function () { button__start.innerHTML = "▶️"; });
-EventEmitter.listen("PauseClock", function () { button__start.innerHTML = "⏸️"; });
+export var button__configurations = document.getElementById("button__configurations");
+// Emiting events
+button__start.addEventListener("click", function () {
+    EventEmitter.emit("StartClock", null);
+});
+button__configurations.addEventListener("click", function () {
+    EventEmitter.emit("OpenConfigurations", null);
+});
