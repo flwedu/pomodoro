1dd8a987ab70d6aeedca9a53c12a7809f4936747
Eduardo Aquino <edutraquino@gmail.com>
2021-11-06 16:33:03 -0300

    created button start/pause

--- src/buttons/Buttons.ts
+++ src/buttons/Buttons.ts
@@ -1,13 +1,9 @@
 import { EventEmitter } from "../core/EventEmitter.js";
 
+export const button__start: HTMLElement = document.getElementById("button__start");
 export const button__focus: HTMLElement = document.getElementById("button__focus");
 export const button__relax: HTMLElement = document.getElementById("button__relax");
 export const button__end: HTMLElement = document.getElementById("button__end");
 
-export const updateStartButton = (clockIsRunning: boolean) => {
-
-    if (clockIsRunning) return "⏸️ Pause Focus";
-    return "⏳ Start Focus";
-}
-
-EventEmitter.listen("StartClock", () => { });
+EventEmitter.listen("StartClock", () => { button__start.innerHTML = "▶️" });
+EventEmitter.listen("PauseClock", () => { button__start.innerHTML = "⏸️" });
