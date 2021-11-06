52542136f852b5bac77431fa202beaf960037829
Eduardo Aquino <edutraquino@gmail.com>
2021-11-06 16:43:52 -0300

    removing button__focus and button__relax

--- src/buttons/Buttons.ts
+++ src/buttons/Buttons.ts
@@ -1,8 +1,6 @@
 import { EventEmitter } from "../core/EventEmitter.js";
 
 export const button__start: HTMLElement = document.getElementById("button__start");
-export const button__focus: HTMLElement = document.getElementById("button__focus");
-export const button__relax: HTMLElement = document.getElementById("button__relax");
 export const button__end: HTMLElement = document.getElementById("button__end");
 
 EventEmitter.listen("StartClock", () => { button__start.innerHTML = "▶️" });
