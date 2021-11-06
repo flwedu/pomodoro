bfdd25fbeae9d4f741da1f81901b2c8574796bef
Eduardo Aquino <edutraquino@gmail.com>
2021-11-06 17:35:10 -0300

    mv buttons listeners to app

--- src/buttons/Buttons.ts
+++ src/buttons/Buttons.ts
@@ -2,6 +2,14 @@ import { EventEmitter } from "../core/EventEmitter.js";
 
 export const button__start: HTMLElement = document.getElementById("button__start");
 export const button__end: HTMLElement = document.getElementById("button__end");
+export const button__configurations = document.getElementById("button__configurations");
 
-EventEmitter.listen("StartClock", () => { button__start.innerHTML = "▶️" });
-EventEmitter.listen("PauseClock", () => { button__start.innerHTML = "⏸️" });
+
+// Emiting events
+button__start.addEventListener("click", () => {
+    EventEmitter.emit("StartClock", null);
+})
+
+button__configurations.addEventListener("click", () => {
+    EventEmitter.emit("OpenConfigurations", null);
+});
