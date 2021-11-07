351dfac0931fd05e52ff44f8a866d64678591c45
Eduardo <edutraquino@gmail.com>
2021-11-07 10:02:39 -0300

    exporting configurations

--- src/configurations.ts
+++ src/configurations.ts
@@ -1,7 +1,16 @@
+import { EventEmitter } from "./core/EventEmitter.js";
 import { hideElement, revealElement } from "./viewFunctions.js";
 
 const saveButton = document.getElementById("button__save");
 const window = document.getElementById("configurations");
+
+const time__focus: any = document.getElementById("time__focus");
+const time__relax: any = document.getElementById("time__relax");
+
+export const configurations = {
+    focusTime: 25,
+    relaxTime: 5
+}
 
 export const openConfigurationsWindow = () => {
     revealElement(window);
@@ -11,6 +20,14 @@ const closeConfigurationWindow = () => {
     hideElement(window);
 }
 
+function getNewConfigurations() {
+
+    configurations.focusTime = Number.parseInt(time__focus.value);
+    configurations.relaxTime = Number.parseInt(time__relax.value);
+}
+
 saveButton.addEventListener("click", () => {
+    getNewConfigurations();
+    EventEmitter.emit("SaveConfigurations", configurations);
     closeConfigurationWindow();
 });
